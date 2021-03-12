2ae679fcab290378f9d15d88f7f93854a90fbbdb
Ben Keys <bkeys818@gmail.com>
2021-03-12 08:14:04 -0500

    Initial Commit

---
+++ U-Map/U_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  U_MapApp.swift
+//  U-Map
+//
+//  Created by Benjamin Keys on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct U_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
