7f57cf7ee72526243f18350dda97de8bc0c13926
Ben Keys <bkeys818@gmail.com>
2021-03-26 10:41:01 -0400

    :wastebasket: Turn off user tracking

--- Campus Map/Main View/MapView.swift
+++ Campus Map/Main View/MapView.swift
@@ -9,52 +9,49 @@ import SwiftUI
 import MapKit
 
 
-// TODO: Move Map to sepretate view (to make data handeling easier) and then use as ZStack
-
 struct MapView: View {
+//    @StateObject private var manager: LocationManager
+    @State private var region: MKCoordinateRegion
+//    @State private var userTrackingMode: MapUserTrackingMode = .none
     private var places: [Place]
-//    @State private var regions: MKCoordinateRegion
-    @StateObject private var manager: LocationManager
-    
-    @State private var userTrackingMode: MapUserTrackingMode = .none
-    
-    let marker = UIImage(named: "Marker")!.withTintColor(.red)
     
     init(region: MKCoordinateRegion, places: [Place]) {
         self.places = places
-        self._manager = StateObject(wrappedValue: LocationManager(region: region))
+        self._region = State(initialValue: region)
+//        self._manager = StateObject(wrappedValue: LocationManager(region: region))
     }
     
     var body: some View {
-        Map(coordinateRegion: $manager.region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $userTrackingMode, annotationItems: places) { place in
+//        Map(coordinateRegion: $manager.region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $userTrackingMode, annotationItems: places) { place in
+        Map(coordinateRegion: $region, interactionModes: .all, annotationItems: places) { place in
             MapMarker(coordinate: place.coordinate)
         }
     }
     
     
-    private class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-        @Published var region = MKCoordinateRegion()
-        private let manager = CLLocationManager()
-        private let regionBounds: MKCoordinateRegion
-        init(region: MKCoordinateRegion) {
-            self.regionBounds = region
-            super.init()
-            manager.delegate = self
-            manager.desiredAccuracy = kCLLocationAccuracyBest
-            manager.requestWhenInUseAuthorization()
-            manager.startUpdatingLocation()
-        }
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            locations.last.map {_ in
-                region = regionBounds
-                print("_")
+//    private class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+//        @Published var region = MKCoordinateRegion()
+//        private let manager = CLLocationManager()
+//        private let regionBounds: MKCoordinateRegion
+//        init(region: MKCoordinateRegion) {
+//            self.regionBounds = region
+//            super.init()
+//            manager.delegate = self
+//            manager.desiredAccuracy = kCLLocationAccuracyBest
+//            manager.requestWhenInUseAuthorization()
+//            manager.startUpdatingLocation()
+//        }
+//
+//        // Triggered when users location updatess
+//        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//            locations.last.map {
 //                let center = CLLocationCoordinate2D(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude)
 //                let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
 //                region = MKCoordinateRegion(center: center, span: span)
-            }
-        }
-        
-    }
+//            }
+//        }
+//
+//    }
 }
 
 //struct MapView_Previews: PreviewProvider {
